bff75d31402aef3f2db7968f7aee45ee48a145c5
sist <sist@DESKTOP-Q88SLS4>
2025-10-13 17:00:09 +0900

    1013

---
+++ JSPBasicProject_1/src/main/webapp/jsp/basic_3.jsp
@@ -0,0 +1,30 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <!-- 
+       => JSP는 실행
+          메모리에 적재 => 파일 이동 / 새로고침
+                        메모리 삭제 _jspDestroy()
+       => 71P
+          _jspInit() => 설정 파일(web.xml)
+          _jspDestroy() => 메모리 해제 : 자동 호출
+          _jspService() => 요청 처리 => 화면 출력(HTML구현)
+          ------------- 비워둔다 (JSP를 이용해 채운다)
+          JSP는 메소드 영역에 코딩
+          _jspService()
+          {
+                
+              JSP파일로 만든다
+                
+          }
+                        
+     -->
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<h1>Hello JSP!</h1>
+</body>
+</html>
---
+++ JSPBasicProject_2/src/main/java/com/sist/dao/EmpDAO.java
@@ -0,0 +1,44 @@
+package com.sist.dao;
+import java.util.*;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+/*
+ * 2 => ibatis => open source
+ * 3 => mybatis
+ * 
+ */
+import java.io.*;
+import com.sist.vo.*;
+public class EmpDAO {
+	private static SqlSessionFactory ssf;
+	static
+	{
+		// XML에 등록된 데이터 읽기 => XML(파싱)
+		try
+		{
+			Reader reader=Resources.getResourceAsReader("Config.xml");
+			ssf=new SqlSessionFactoryBuilder().build(reader);
+		}catch(Exception ex) {}
+	}
+	// 기능 설정
+	public static List<EmpVO> empListData()
+	{
+		SqlSession session=ssf.openSession();
+		List<EmpVO> list=session.selectList("empListData");
+		session.close(); // 반환 => disConnection()
+		return list;
+	}
+	//            resultType           parameterType
+	public static EmpVO empDetailData(int empno)
+	{
+		SqlSession session=ssf.openSession();
+		EmpVO vo=session.selectOne("empDetailData",empno);
+		session.close(); // 반환 => disConnection()
+		return vo;
+	}
+}
+
